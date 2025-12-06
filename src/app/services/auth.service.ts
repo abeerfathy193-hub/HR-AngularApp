9709f43ce024b4d4436f198cca35b4da0248dead
Ahmedhatem307 <ahmedhatem.aez@gmail.com>
2025-12-06 04:05:04 +0200

    Refactor leave type and auth services for consistency
    Updated import path for LeaveTypeService to use consistent casing and improved formatting in leavetypscomponant.ts. Refactored AuthService to simplify currentUser structure, fixed user property access, and improved code style for better maintainability.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,19 +4,17 @@ import { Router } from '@angular/router';
 import { LoginRequest } from '../interfaces/login.interface';
 import { environment } from '../../environments/environment.development';
 
-
-
 export interface LoginResponse {
   user: {
     userName: string;
-    imagepath: string
+    imagepath: string;
     roles: string[];
-  }
+  };
   token: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private readonly API_URL = `${environment.webApiURL}/api/Account`;
@@ -25,13 +23,12 @@ export class AuthService {
   private _currentUser = signal<LoginResponse['user'] | null>(null);
 
   isAuthenticated: boolean = false;
-  currentUser: LoginResponse | null = null;
+  currentUser: { userName: string; imagepath: string | null; roles: string[] } | null = null;
   token: string | null = null;
 
   constructor(private http: HttpClient, private router: Router) {
     this.checkStoredAuth();
   }
-
 
   private checkStoredAuth(): void {
     const token = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
@@ -45,25 +42,25 @@ export class AuthService {
     }
   }
   getUserRoles(): string[] | null {
-    return this.currentUser ? this.currentUser.user.roles : null;
+    return this.currentUser ? this.currentUser.roles : null;
   }
 
   getUserName(): string | null {
-    return this.currentUser ? this.currentUser.user.userName : null;
+    return this.currentUser ? this.currentUser.userName : null;
   }
   getUserImage(): string | null {
-    return this.currentUser ? `${environment.webApiURL}/Files/${this.currentUser.user.imagepath}` : null;
+    return this.currentUser ? `${environment.webApiURL}/Files/${this.currentUser.imagepath}` : null;
   }
   login(credentials: LoginRequest) {
     return this.http.post<LoginResponse>(`${this.API_URL}/login`, credentials);
   }
   setCredentials(credentials: LoginResponse, rememberMe: boolean = false) {
+    debugger;
     const storage = rememberMe ? localStorage : sessionStorage;
     storage.setItem('authToken', credentials.token);
     storage.setItem('currentUser', JSON.stringify(credentials.user));
     this.checkStoredAuth();
   }
-
 
   register(registerData: FormData) {
     return this.http.post(`${environment.webApiURL}/api/Applicant/Register`, registerData);
@@ -82,4 +79,3 @@ export class AuthService {
     sessionStorage.removeItem('currentUser');
   }
 }
-
